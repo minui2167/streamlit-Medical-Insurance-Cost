97791082b94ee76d709d35cce47193417861199d
minui2167 <minui2167@gmail.com>
2022-05-24 18:07:42 +0900

    add

---
+++ app_eda.py
@@ -0,0 +1,41 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def run_eda():
+    df = pd.read_csv('insurance.csv')
+ 
+    st.subheader('연간 보험비 분포 그래프')
+    fig = plt.figure()
+    sns.set(style='whitegrid')
+    sns.histplot(df['charges'], kde = True, color = 'c')
+    plt.title('Distribution of Charges')
+    st.pyplot(fig)
+    st.text('10000달러 이하가 많고 20000달러 이상은 적다.')
+
+    st.subheader('4개의 지역간 차이는 어떻게 될까?')
+    fig = plt.figure()
+    charges = df['charges'].groupby(df.region).sum().sort_values(ascending = True)
+    sns.barplot(charges, charges.index, palette ='Blues')
+    st.pyplot(fig)
+    st.text('남동부는 50000달러가 넘고 나머지는 40000달러대다.')
+
+    st.subheader('성별간 차이는 얼마나 날까?')
+    fig = plt.figure()
+    sns.barplot(x ='region', y ='charges', hue = 'sex', data = df, palette ='cool', ci = None)
+    st.pyplot(fig)
+    st.text('북서부를 빼면 남자가 대체로 더 비싸다.')
+
+    st.subheader('흡연 여부에 따라 얼마나 다를까?')
+    fig = plt.figure()
+    sns.barplot(x = 'region', y = 'charges', hue='smoker', data=df, palette='Reds_r', ci = None)
+    st.pyplot(fig)
+    st.text('4개 지역 모두 흡연자가 더 비싸다.')
+
+    st.subheader('자녀수에 따라 얼마나 다를까?')
+    fig = plt.figure()
+    sns.barplot(x='region', y='charges', hue='children', data=df, palette='Set1', ci = None)
+    st.pyplot(fig)
+    st.text('뚜렷한 차이는 보이지 않는다.')
---
+++ app_home.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pandas as pd
+
+def run_home():
+    st.title('미국 의료 보험비에 영향을 미치는 요소들과 보험비 예상')
+
+    url = 'https://i.imgur.com/zTnvOcb.jpg'   
+    st.image(url)
+
+    st.text('나이, 성별, bmi, 자녀수, 흡연 여부, 지역에 따라 보험비는 어떻게 다를까요?\n위 요소들에 따라 보험비를 예측해 봅시다.')
+
+    df = pd.read_csv('insurance.csv')
+    st.subheader('데이터 미리보기')
+    st.dataframe(df)
+
+    st.subheader('수치형 데이터 요약')
+    st.dataframe(df.describe())
