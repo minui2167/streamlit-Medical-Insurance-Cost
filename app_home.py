967d6558f3ad83e098f2acc22bf07e778b58f3d7
minui2167 <minui2167@gmail.com>
2022-05-29 18:21:38 +0900

    Update app_home.py

--- app_home.py
+++ app_home.py
@@ -2,12 +2,12 @@ import streamlit as st
 import pandas as pd
 
 def run_home():
-    st.subheader('미국 의료 보험비에 영향을 미치는 요소들과 보험비 예상')
+    st.subheader('의료 보험비에 영향을 미치는 요소들과 보험비 예상')
 
     url = 'https://i.imgur.com/zTnvOcb.jpg'   
     st.image(url)
 
-    st.text('나이, 성별, bmi, 자녀수, 흡연 여부, 지역에 따라 보험비는 어떻게 다를까요?\n위 요소들에 따라 보험비를 예측해 봅시다.')
+    st.text('나이, 성별, bmi, 자녀수, 흡연 여부, 지역에 따라 보험비(달러)는 어떻게 다를까요?\n위 요소들에 따라 보험비를 예측해 봅시다.')
 
     df = pd.read_csv('data/insurance.csv')
     st.subheader('데이터 미리보기')
